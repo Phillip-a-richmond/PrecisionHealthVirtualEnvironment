6cd18ecc05d02c589db5761416bd7b0dee3b7179
singha53 <amrit.singh@hli.ubc.ca>
2022-08-05 05:05:35 -0700

    add custom lib path

--- Workshops/omics_exploration/get_rpystudio.sh
+++ Workshops/omics_exploration/get_rpystudio.sh
@@ -3,4 +3,16 @@ IMAGE_VERSION=v0.1
 
 module load gcc singularity
 
+# Change to your directory where you'll keep the SIF 
+Home_Dir=/scratch/st-sturvey-1/Workshops/StudentSpaces/singha53/PrecisionHealthVirtualEnvironment/Workshops/omics_exploration/
+
+mkdir -p $Home_Dir
+cd $Home_Dir
+
+# Make an R_Lib directory there that matches the R version. To find the R version read up on the docker tag (link below)
+mkdir -p $Home_Dir/R_Libs_4.1.0/
+
+# Set your singularity cache
+export SINGULARITY_CACHEDIR=$PWD
+
 singularity pull --name rpystudio.sif docker://$DOCKERHUB_USERNAME/rpystudio:$IMAGE_VERSION
