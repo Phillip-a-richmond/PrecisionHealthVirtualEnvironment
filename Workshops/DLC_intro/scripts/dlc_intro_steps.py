6bec2696523a3cf8c0c08f4de2dd61cc1adb8807
phillip-a-richmond <phillip.a.richmond@gmail.com>
2022-08-08 17:57:44 -0700

    pankaj updated scripts for dlc

--- Workshops/DLC_intro/scripts/dlc_intro_steps.py
+++ Workshops/DLC_intro/scripts/dlc_intro_steps.py
@@ -1,20 +1,19 @@
 
 import deeplabcut as dlc
 
-config_path = dlc.create_new_project('Project_name', 'Experimenter', ['videofile_path'], working_directory = 'path_where_you_want_the_project_files', videotype='mp4', copy_videos = True)
+#config_path = dlc.create_new_project('Project_name', 'Experimenter', ['videofile_path'], working_directory = 'path_where_you_want_the_project_files', videotype='mp4', copy_videos = True)
+config_path = dlc.create_new_project('test', 'me', ['/scratch/tr-precisionhealth-1/Workshops/StudentSpaces/$CWL/DLC_intro/data/00029.avi'], working_directory = '/scratch/tr-precisionhealth-1/Workshops/StudentSpaces/$CWL/DLC_intro/projects/', videotype='avi', copy_videos = True)
 
 dlc.extract_frames(config_path, 'automatic','kmeans',crop=False)
 
 dlc.label_frames(config_path)
 
-dlc.cropimagesandlabels(config_path)
-
 dlc.create_training_dataset(config_path)
 
-dlc.train_network(config_path, saveiters = 2500, maxiters = 10000, displayiters = 100)
+dlc.train_network(config_path, saveiters = 250, maxiters = 1000, displayiters = 100)
 
 dlc.evaluate_network(config_path)
 
-dlc.analyze_videos(config_path, ['file_directory_of_file_1'], save_as_cv = True)
+dlc.analyze_videos(config_path, ['/scratch/tr-precisionhealth-1/Workshops/StudentSpaces/$CWL/DLC_intro/data/'], save_as_cv = True)
 
-dlc.create_labeled_video(config_path, ['final_file_directory_of_file_1'])
+dlc.create_labeled_video(config_path, ['/scratch/tr-precisionhealth-1/Workshops/StudentSpaces/$CWL/DLC_intro/data/'])
